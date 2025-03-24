6523a5e39344bc279b3f91226b479be216a7616e
Ricci.rudenko <richardrudenko555@gmail.com>
2025-03-24 10:47:32 +0100

    Layout changes. Redirect.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,11 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(): Response
     {
+        if(!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
